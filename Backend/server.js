ba88d223729cbd81801d29caa05508b373b4f2d3
Tarun <tarunvashisth0000@gmail.com>
2025-03-21 17:34:34 +0530

    Commit

--- Backend/server.js
+++ Backend/server.js
@@ -28,4 +28,3 @@ app.use('/favorites', favoriteRoutes);
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`🚀 Server running on port ${PORT}`));
 
-module.exports = app;
--- Backend/src/models/userModel.js
+++ Backend/src/models/userModel.js
@@ -1,10 +1,14 @@
 const mongoose = require("mongoose");
 
 const UserSchema = new mongoose.Schema({
-  name: String,
-  email: { type: String, unique: true },
-  password: String,
-  role: { type: String, default: "user" },
+  name: { type: String, required: true },
+  email: { type: String, required: true, unique: true },
+  password: { type: String, required: true },
+  role: { 
+    type: String, 
+    enum: ["user", "admin"],
+    required: true 
+  },
   favorites: [{ type: mongoose.Schema.Types.ObjectId, ref: "Book" }]
 });
 
