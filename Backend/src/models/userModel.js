40238f9385551b5fa6a2015b166cfa4a962ac8bb
Tarun <tarunvashisth0000@gmail.com>
2025-03-21 19:38:49 +0530

    Commit

--- Backend/src/models/userModel.js
+++ Backend/src/models/userModel.js
@@ -1,10 +1,10 @@
 const mongoose = require("mongoose");
 
 const UserSchema = new mongoose.Schema({
-  name: String,
-  email: { type: String, unique: true },
-  password: String,
-  role: { type: String, default: "user" },
+  name: { type: String, required: true },
+  email: { type: String, unique: true, required: true },
+  password: { type: String, required: true },
+  role: { type: String, enum: ["user", "admin"], required: true }, 
   favorites: [{ type: mongoose.Schema.Types.ObjectId, ref: "Book" }]
 });
 
